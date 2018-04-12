9a537e6ba4ce01bd4789a407aaab839ce64a5970
Parker Wolcott <parker.wolcott@msubillings.edu>
2018-04-12 11:02:51 -0600

    Commit

---
+++ AdventureGame/AdventureGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+///Website to be used
+///http://programmingisfun.com/learn/c-sharp-adventure-game/c_sharp_04_input/
+///
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CharacterName = "John Doe";
+            Console.WriteLine("Adventure Game!");
+            Console.WriteLine("Welcome to your Adventure!");
+            Console.WriteLine("What would you like your character's name to be?");
+            CharacterName = Console.ReadLine();
+            Console.ReadKey();
+            Console.WriteLine("Great! Your character is now named " + CharacterName);
+            Console.ReadLine();
+        }
+    }
+}
