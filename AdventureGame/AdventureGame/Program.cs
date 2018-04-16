b4dd106663d51366a12228e7b6c6a281e0c013c6
Parker Wolcott <parker.wolcott@msubillings.edu>
2018-04-16 13:10:06 -0600

    Commit

--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -12,35 +12,228 @@ http://programmingisfun.com/learn/c-sharp-adventure-game/
 namespace AdventureGame
 {
     public static class Game {
+
+        static int Scenarios = 3;
         //Character name
-        static string CharacterName = "John Doe";
+        static string CharacterName;
+
+
+        static string[] PartOne = {
+
+            #region Choice 1
+            "You're strolling down your left path that you chose. You hear screaming coming from the distance off to the direction of the path on the right. You're extremely happy you chose the left path",
+            "You're casually walking down your path and are suddenly ambushed by a group of bandits hiding behind the tree. They cut you to pieces and you die.",
+            #endregion Choice 1
+
+            #region Choice 2
+            "You've chosen to help the child out by walking her the way you came. She gives out some kind of signal and you're suddenly surrounded by bandits waiting for her to bring their next victum. You've reached the end of your journey",
+            "You choose to leave the girl behind and hope she can find her way back",
+            #endregion Choise 2
+
+            "After you leave the girl, you continue along your path and encounter an old building that looks abandon",
+        };
+
+        static string[] PartTwo = {
+
+        };
+
+        static string[] PartThree = {
+
+        };
 
 
         //Print out game title and overview
         public static void StartGame()
         {
-            Console.WriteLine("Adventure Game!");
-            Console.WriteLine("Welcome to your Adventure!");
+            GameTitle();
             NameCharacter();
+            Choice();
+            EndGame();
+
         }
 
+        public static void EndGame()
 
-        //Ask player for a name and save it
-        static void NameCharacter()
         {
-            Console.WriteLine("What would you like your character's name to be?");
+            Console.WriteLine("End of story text here.....");
+            Console.WriteLine("Congratulations " + CharacterName + "!");
+            Console.ReadLine();
+            
+
+        }
+
+        static void Choice()
+        {
+            for (int section = 1; section <= Scenarios; section++)
+            {
+                string input = "";
+                
+                switch (section)
+                {
+                    case 1:
+                        Console.WriteLine("You're walking down a forest path and come to a fork in the road. You have a decision to make. " + CharacterName + ", which path will you choose? Left or Right?");
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("Enter Your Choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+
+                        if (input == "left")
+                        {
+                            Console.WriteLine(PartOne[0]);
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartOne[1]);
+                            Console.ReadLine();
+                            return;
+                        }
+
+                        Console.WriteLine("You reach the end of your path and are approched by a young child who tells you that she's lost her way." + CharacterName + ", Do you choose to help her or hope she finds her way? Help or Leave?");
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("Enter Your Choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+
+                        if (input == "help")
+                        {
+                            Console.WriteLine(PartOne[2]);
+                            Console.ReadLine();
+                            return;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartOne[3]);
+                            Console.ReadLine();
+
+                        }
+
+                        Console.WriteLine(PartOne[4]);
+
+                        break;
+
+
+
+                    case 2:
+                        Console.WriteLine(PartTwo[0]);
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("Enter Your Choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartTwo[1]);
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartTwo[2]);
+                            Console.ReadLine();
+                        }
+
+                        Console.WriteLine(PartTwo[3]);
+
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartTwo[4]);
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartTwo[5]);
+                            Console.ReadLine();
+
+                        }
+
+                        Console.WriteLine(PartTwo[6]);
+
+                        break;
+
+
+
+                    case 3:
+                        Console.WriteLine(PartThree[0]);
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("Enter Your Choice: ");
+                        input = Console.ReadLine();
+                        input = input.ToLower();
+                        Console.ResetColor();
+
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartThree[1]);
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartThree[2]);
+                            Console.ReadLine();
+                        }
+
+                        Console.WriteLine(PartThree[3]);
+
+                        if (input == "a")
+                        {
+                            Console.WriteLine(PartThree[4]);
+                            Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            Console.WriteLine(PartThree[5]);
+                            Console.ReadLine();
+
+                        }
+
+                        Console.WriteLine(PartThree[6]);
+
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static void NameCharacter()
+
+        {
+
+            Console.WriteLine("What is your Characters name for this adventure? ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Enter your name: ");
             CharacterName = Console.ReadLine();
-
-            Console.WriteLine("Great! Your character is now named " + CharacterName);
-            Console.WriteLine("Welcome, " + CharacterName + ", and good luck!\n\n");
+            Console.ResetColor();
+            Console.WriteLine("Your name is confirmed to be " + CharacterName + ". Good luck!\n\n");
         }
 
-        //original Dialog method
-        public static void Dialog(string message)
+        static void GameTitle()
         {
+            string Title = @"ADVENTURE GAME";
+            Console.Title = Title;
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine(message);
+            Console.WriteLine(Title);
             Console.ResetColor();
+            Console.WriteLine("Press enter to start");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 
@@ -53,11 +246,6 @@ namespace AdventureGame
         {
             Game.StartGame();
             Console.ReadKey();
-
-            Console.Write("A stranger approaches. Clearing their throat to get your attention, they ask \n");
-            Game.Dialog("Have you seen a strange creature around here? \nAbout three feet high, greenish, with fluffy hair? \n");
-            Console.Write("How do you answer? 1) Yes, 2) No, 3) Repeat the question, 4) Keep walking\n");
-            Console.ReadLine();
         }
     }
 }
